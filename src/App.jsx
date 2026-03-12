45ca137a9c87122034e5f25a4378a165c3f1b6fc
estefany2239 <estefanyarango533@gmail.com>
2026-03-12 16:05:46 -0500

    nueva funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import { useState } from 'react';
 //import './shared/styles/App.css';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import {HashRouter, Routes, Route } from "react-router-dom";
 //components layout
 import { Header } from './features/layout/components/Header';
 import { Footer } from './features/layout/components/Footer';
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Header />
           <Routes>
             <Route path="/" element={<Content/>}></Route>
@@ -30,7 +30,7 @@ function App() {
             
           </Routes>
         <Footer />
-      </BrowserRouter>   
+      </HashRouter>   
     </>
   );
 }
